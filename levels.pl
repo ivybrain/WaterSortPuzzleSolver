4488310b11e283bac76bda979cdd5e9b478f9a64
Ivy Brain <ivymbrain@gmail.com>
2020-12-04 11:06:36 +1100

    Initial commit

---
+++ levels.pl
@@ -0,0 +1,148 @@
+
+%Levels are lists of lists, with each tube listed as its colours, top to bottom, or empty
+
+level_3([
+[violet, red, orange, violet],
+[violet, red, orange, orange],
+[red, orange, violet, red],
+[],
+[]
+]).
+
+level_5([
+[pink, violet, orange, green],
+[pink, violet, green, orange],
+[red, orange, red, pink],
+[violet, red, pink, orange],
+[violet, red, green, green],
+[],
+[]
+]).
+
+level_108([
+[lightblue, green, red, lightgreen],
+[lightblue, lightblue, green, pink],
+[red, red, green, purple],
+[violet, pink, orange, grey],
+[pink, red, grey, purple],
+[lightgreen, grey, purple, violet],
+[orange, purple, green, violet],
+[orange, grey, orange, lightgreen],
+[pink, lightgreen, violet, lightblue],
+[],
+[]
+]).
+
+test_level([
+[red, red, yellow, yellow],
+[yellow, yellow, red, red],
+[]
+]).
+
+
+level_10([
+[violet, green, violet, pink],
+[red, pink, grey, orange],
+[green, blue, blue, violet],
+[green, orange, orange, pink],
+[red, green, grey, grey],
+[blue, blue, red, violet],
+[grey, orange, pink, red],
+[],
+[]
+
+]).
+
+level_31([
+[purple, pink, green, violet],
+[violet, olive, purple, blue],
+[orange, green, grey, violet],
+[blue, orange, orange, pink],
+[pink, red, red, grey],
+[green, olive, orange, violet],
+[blue, purple, red, green],
+[olive, red, olive, grey],
+[purple, grey, blue, pink],
+[],
+[]
+]).
+
+
+level_133([
+[purple, olive, brown, lightgreen],
+[yellow, green, green, purple],
+[red, brown, lightgreen, pink],
+[lightblue, red, violet, grey],
+[grey, lightgreen, purple, orange],
+[lightblue, lightblue, green, yellow],
+[orange, purple, violet, red],
+[olive, violet, yellow, red],
+[pink, green, violet, pink],
+[lightgreen, lightblue, brown, pink],
+[grey, orange, brown, olive],
+[yellow, orange, grey, olive],
+[],
+[]
+]).
+
+level_135([
+[red, orange, red, pink],
+[purple, blue, pink, grey],
+[yellow, lightblue, green, brown],
+[yellow, green, green, purple],
+[pink, olive, lightgreen, red],
+[pink, brown, grey, olive],
+[lightblue, lightgreen, yellow, lightgreen],
+[blue, red, orange, olive],
+[lightblue, brown, yellow, green],
+[orange, orange, grey, lightblue],
+[grey, brown, purple, purple],
+[blue, lightgreen, blue, olive],
+[],
+[]
+]).
+
+level_136([
+[purple, red, grey, olive],
+[blue, blue, green, violet],
+[grey, orange, orange, green],
+[orange, purple, pink, grey],
+[olive, purple, pink, olive],
+[violet, blue, purple, red],
+[blue, grey, orange, green],
+[olive, red, green, violet],
+[pink, violet, pink, red],
+[],
+[]
+]).
+
+level_137([
+[purple, yellow, blue, lightgreen],
+[purple, pink, orange, green],
+[green, yellow, brown, pink],
+[red, orange, green, pink],
+[green, olive, red, orange],
+[blue, yellow, blue, brown],
+[pink, grey, violet, grey],
+[purple, red, red, orange],
+[purple, violet, violet, violet],
+[blue, lightgreen, lightgreen, olive],
+[grey, olive, lightgreen, yellow],
+[grey, brown, olive, brown],
+[],
+[]
+]).
+
+level_138([
+[purple, pink, olive, blue],
+[pink, violet, pink, orange],
+[grey, grey, violet, purple],
+[red, orange, green, olive],
+[green, violet, blue, red],
+[pink, blue, olive, green],
+[olive, purple, blue, grey],
+[violet, purple, red, orange],
+[grey, green, orange, red],
+[],
+[]
+]).
---
+++ watersort.pl
@@ -0,0 +1,127 @@
+
+% verify_level(+Level)
+% Ensures that each colour listed in the level occurs 4 times
+verify_level(Level) :-
+  call(Level, X),
+  flatten(X, Flat),
+  sort(Flat, Colours),
+  findall(N, (member(C, Colours), count_elem(C, Flat, N)), Counts),
+  all_same(Counts, 4).
+
+% Counts the occerances of Elem in List
+% count_elem(+Elem, +List, -N)
+count_elem(_, [], 0).
+count_elem(Elem, [L|List], N) :-
+  L == Elem ->
+    count_elem(Elem, List, N1),
+    N is N1 + 1
+  ;
+  count_elem(Elem, List, N).
+
+
+% Finds a series of moves that takes a Level to a winning state
+% solve_level(+Level, ?Moves)
+solve_level(Level, Moves) :-
+  verify_level(Level) ->
+    call(Level, Tubes),
+    findall((N, Tube), (member(Tube, Tubes), nth1(N, Tubes, Tube)), S),
+    sort(1, @<, S, State),
+    make_move(State, [], Moves)
+  ;
+    throw("Invalid Level Data").
+
+% Finds a series of valid moves that lead to a winning state, then return these moves
+% make_move(+State, +Acc, - Moves)
+make_move(State, Acc, Moves) :-
+  win_state(State),
+  reverse(Acc, Moves).
+
+make_move(State, Acc, Moves) :-
+  member((Fn, From), State),
+  member((Tn, To), State),
+  Tn \= Fn,
+  valid_move((Fn, From), (Tn,To), (Fn, FromNew), (Tn, ToNew)),
+  sort(1, @<, [(Fn, FromNew), (Tn, ToNew)], Changes),
+  sort(1, @<, State, StateS),
+  update_state(StateS, Changes, [], State_next),
+  %not_in_past(State_next, PastStates),
+  %make_move(State_next, [(Fn, Tn)|Acc], Moves, [StateS|PastStates]).
+  make_move(State_next, [(Fn, Tn)|Acc], Moves).
+
+
+
+% Takes a state (list of (TubeNumber, Tube)) and creates a new one with the elements in Changes (a similar list)
+% replacing the corresponding elements from the old state
+% update_state(+OldState, +Changes, +Acc, -NewState)
+update_state([],_ , Acc, Acc).
+update_state(State, [], Acc, New) :-
+  append(Acc, State, New).
+update_state([(N,Elem)|Old], [(Nc, Change)|Rest], Acc, New) :-
+  N == Nc ->
+    update_state(Old, Rest, [(Nc, Change)|Acc], New)
+  ;
+  update_state(Old, [(Nc, Change)|Rest], [(N,Elem)|Acc], New).
+
+
+
+
+%Checks if a pour from From to To is valid, and finds the result afterwards
+% valid_move(?from, ?to, ?FromNew, ?ToNew)
+valid_move((Fn,From), (Tn,[T|To]), (Fn, FromNew), (Tn, ToNew)) :-
+  append(F, FromNew, From),
+  F \= [],
+  all_same(F, C),
+  ([R|_] = FromNew ->
+    R \= C
+  ; true()),
+
+  C == T,
+  append(F, [T|To], ToNew),
+  length(ToNew, Len),
+  Len =< 4.
+
+valid_move((Fn, From), (Tn, []), (Fn, FromNew), (Tn, ToNew)) :-
+  % Pouring a single colour into empty is pointless
+  \+ all_same(From, _),
+  append(F, FromNew, From),
+  F \= [],
+  all_same(F, C),
+  ([R|_] = FromNew ->
+    R \= C
+  ; true()),
+  append(F, [], ToNew),
+  length(ToNew, Len),
+  Len =< 4.
+
+% We win if all the tubes have 1 colour, and they are all either empty or full
+% win_state(State)
+win_state([]).
+win_state([(_,Tube)|Rest]) :-
+  all_same(Tube, _),
+  length(Tube, N),
+  (N == 0 ; N == 4),
+  win_state(Rest).
+
+
+% Matches if every element in List is Value
+% all_same(+List, ?Value)
+all_same([], _).
+all_same([X|Xs], X) :-
+  all_same(Xs, X).
+
+
+
+
+% Unused, was used to keep track of past states
+not_in_past(State, PastStates) :-
+  sort(1, @<, State, StateS),
+  \+ has_permutation(StateS, PastStates).
+
+has_permutation(State, PastStates) :-
+  member(PastState, PastStates),
+  is_permutation(State, PastState).
+
+is_permutation([], _).
+is_permutation([(_, Tube)|Ss], S2) :-
+  member((_, Tube), S2),
+  is_permutation(Ss, S2).
